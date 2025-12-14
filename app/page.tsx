53cfcf6d8ce10760cdf8cf16e2f2659380af1f9e
yavchris07 <wilondjayav27@gmail.com>
2025-12-14 10:15:27 +0200

    initialize duqah stat

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,34 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans">
+    <div className="">
+      <h1 className="text-2xl font-bold text-zinc-800">Duqah Stat</h1>
+      <p className="mt-4 text-[#558455]">
+        La statistique de vos produits pour mieux decider.
+      </p>
+      <div className="mt-2">
+        <label htmlFor="" className="text-gray-400">Telephone</label>
+        <input
+          type="text"
+          className="mt-2 block w-full text-gray-500 rounded border border-zinc-300 bg-white px-3 py-3 shadow-sm placeholder:text-zinc-400 focus:border-[#558455] focus:outline-none focus:ring-1 focus:ring-[#558455]"
+          placeholder="+1 (555) 555-5555"
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </div>
+      <div className="mt-2">
+        <label htmlFor="" className="text-gray-400">Mot de passe</label>
+        <input
+          type="password"
+          className="mt-2 block w-full text-gray-500 rounded border border-zinc-300 bg-white px-3 py-3 shadow-sm placeholder:text-zinc-400 focus:border-[#558455] focus:outline-none focus:ring-1 focus:ring-[#558455]"
+          placeholder="Votre mot de passe"
+        />
+      </div>
+      <div className="mt-4">
+        <button className="w-full rounded bg-[#558455] px-4 py-3 font-medium text-white hover:bg-[#4a744a] transition-colors duration-200">
+          Se connecter
+        </button>
+      </div>
+    </div>
     </div>
   );
 }
